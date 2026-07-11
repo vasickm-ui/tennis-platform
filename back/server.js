0cb906d4ee7d8d376d708cb63b290a183406aea7
vasickm-ui <vasic.km@gmail.com>
2026-07-11 22:23:41 +0200

    connected to db

--- back/server.js
+++ back/server.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 
@@ -6,7 +9,30 @@ const PORT = 3000;
 
 
 app.get("/", (req, res) => {
-    res.send("Backend is running");
+    res.send("Backend ining");
+});
+
+
+// Test database connection
+app.get("/test-db", async (req, res) => {
+
+    try {
+        const result = await db.query("SELECT NOW()");
+
+        res.json({
+            message: "Database connected",
+            time: result.rows[0]
+        });
+
+    } catch(error) {
+
+        console.error(error);
+        res.status(500).json({
+            error: "Database connection failed"
+        });
+
+    }
+
 });
 
 
