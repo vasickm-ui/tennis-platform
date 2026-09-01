07bf3e172bc3f8e1c4d8e207c16274cec12421c1
vasickm-ui <vasic.km@gmail.com>
2026-09-01 16:11:25 +0200

    Add unit test for email and remove bin obj

--- UserService.Tests/UserServiceTests.cs
+++ UserService.Tests/UserServiceTests.cs
@@ -1,8 +1,29 @@
-using Xunit;
+using Moq;
+using UserService.Repositories;
+using UserService.DTOs;
 
 namespace UserService.Tests;
 
 public class UserServiceTests
 {
-    
+    [Fact]
+    public async Task Register_ShouldThrowException_WhenEmailAlreadyExists()
+    {
+        var mockRepository = new Mock<IUserRepository>();
+
+        mockRepository
+            .Setup(r => r.EmailExistsAsync("mile@gmail.com"))
+            .ReturnsAsync(true);
+        
+        var service = new UserService.Services.UserService(mockRepository.Object);
+
+        var request = new RegisterRequestDTO
+        {
+            FirstName = "Mile",
+            LastName = "Jankovic",
+            Email = "mile@gmail.com",
+            Password = "12345678"
+        };
+
+    }
 }
